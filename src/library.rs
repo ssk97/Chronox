c7e24670ac63008f94c0a44daf93be0111f5f74f
ssk97 <ssk97@case.edu>
2018-03-05 00:22:48 -0800

    Finished basic unit functionality, added spawning with RNG stats

--- src/library.rs
+++ src/library.rs
@@ -1,24 +1,41 @@
 pub extern crate cgmath;
 use self::cgmath::*;
-pub fn checkDir <A: Angle>(input_angle: A, target_angle: A) -> i8 {
+use std::ops::Neg;
+pub fn check_dir <T, A>(input_angle: A, target_angle: A) -> T
+    where T: BaseFloat, A: Angle<Unitless = T>, {
     if input_angle < target_angle {
-        if (input_angle - target_angle).normalize() < A::turn_div_2() {
-            return 1;
+        if (target_angle - input_angle).normalize() < A::turn_div_2() {
+            return T::one();
         } else {
-            return -1;
+            return T::zero()-T::one();
         }
     } else {
         if (input_angle - target_angle).normalize() < A::turn_div_2() {
-            return -1;
+            return T::zero()-T::one();
         } else {
-            return 1;
+            return T::one();
         }
     }
 }
-pub fn pt_dir<T: BaseFloat, A: Angle>(from: Point2 <T>, to: Point2 <T>) -> A{
-    let offset: Vector2<T> = from - to;
-    return offset.angle(Vector2::unit_x());
+pub fn pt_dir<T, A>(from: Point2 <T>, to: Point2 <T>) -> A
+where T: BaseFloat, A: Angle<Unitless = T>, {
+    let offset: Vector2<T> = to - from;
+    return A::atan2(offset.y, offset.x);
+
 }
-pub fn lendir<T: BaseFloat, A: Angle>(len: f32, dir: A) -> Vector2 <T>{
-    return dir.sin_cos()*len;
+pub fn lendir<T, A>(len: T, dir: A) -> Vector2 <T>
+where T: BaseFloat, A: Angle<Unitless = T>, {
+    let (y, x) = dir.sin_cos();
+    return vec2(x, y)*len;
+}
+
+pub fn bound<T>(val: T, max: T) -> T
+where T: PartialOrd+Neg<Output = T>+Copy{
+    if val > max{
+        return max;
+    }
+    if val < -max{
+        return -max;
+    }
+    return val;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,141 @@
 //! The simplest possible example that does something.
+extern crate rand;
+use rand::distributions::{IndependentSample, Range};
+use rand::{thread_rng, Rng};
 
 extern crate ggez;
 use ggez::*;
-use ggez::graphics::{DrawMode, Point2};
+use ggez::graphics::{DrawMode};
+use ggez::event::*;
 mod library;
 use library::*;
 use library::cgmath::*;
 
-struct globals{
-    mouse_loc: Point2
-}
-//static globs: globals;
+use ggez::graphics::Point2 as PT;
+#[allow(non_camel_case_types)]
+type pt = Point2<f32>;
+#[allow(non_camel_case_types)]
+type deg = Deg<f32>;
 
-struct boidConstants{
+fn rads(angle: deg) -> f32{
+    return Rad::from(angle).0;
+}
+fn pt_gfx(p: pt) -> PT{
+    return PT::new(p.x, p.y);
+}
+
+
+
+fn boid_mesh(ctx: &mut Context) -> GameResult<graphics::Mesh>{
+    let mb = &mut graphics::MeshBuilder::new();
+    let verts = [PT::new(20., 0.),
+                              PT::new(-20., -5.),
+                              PT::new(-10., 0.),
+                              PT::new(-20., 5.),];
+    mb.polygon(DrawMode::Fill, &verts);
+    return mb.build(ctx);
+}
+
+#[derive(Debug)]
+struct BoidConstants{
     vel: f32,
-    rotAccel: Deg<f32>,
-    rotVelMax: Deg<f32>,
+    rot_accel: deg,
+    rot_vel_max: deg,
     lookahead: f32
 }
-struct boid{
-    loc: library::cgmath::Point2<f32>,
-    rot: Deg<f32>,
-    rotVel: Deg<f32>,
-    consts: boidConstants
+impl BoidConstants{
+    fn new<T: Rng>(rng: &mut T) -> BoidConstants{
+        BoidConstants{
+            vel: Range::new(2., 6.).ind_sample(rng),
+            rot_accel: Deg(Range::new(0.5, 2.).ind_sample(rng)),
+            rot_vel_max: Deg(Range::new(4., 8.).ind_sample(rng)),
+            lookahead: Range::new(5., 15.).ind_sample(rng),
+        }
+    }
 }
-impl boid{
-    fn update(&mut self, target: Point2){
-        let angle: Deg<f32> = pt_dir(self.loc, self.loc);
-        self.loc += lendir(self.consts.vel, angle);
+struct Boid{
+    loc: pt,
+    dir: deg,
+    rot_vel: deg,
+    stats: BoidConstants
+}
+impl Boid{
+    fn new(loc: pt, stats: BoidConstants) -> Boid{
+        Boid{loc, dir:Deg(0.), rot_vel:Deg(0.), stats}
+    }
+    fn update(&mut self, target: pt){
+        let ahead = self.loc + (lendir(self.stats.vel, self.dir)*self.stats.lookahead);
+        let dir = pt_dir(ahead, target);
+        self.rot_vel += self.stats.rot_accel*check_dir(self.dir, dir);
+        self.rot_vel = bound(self.rot_vel, self.stats.rot_vel_max);
+        self.dir += self.rot_vel;
+        self.loc += lendir(self.stats.vel, self.dir);
     }
 }
 
 struct MainState {
-    pos_x: f32,
+    mouse: pt,
+    boids: Vec<Boid>,
 }
 
 impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
-        let s = MainState { pos_x: 0.0 };
+        let consts1 = BoidConstants{vel: 5., rot_accel: Deg(1.), rot_vel_max: Deg(10.), lookahead: 5.};
+        let boid1 = Boid::new(pt{x: 100., y: 100.}, consts1);
+        let consts2 = BoidConstants{vel: 4., rot_accel: Deg(0.5), rot_vel_max: Deg(5.), lookahead: 15.};
+        let boid2 = Boid::new(pt{x: 100., y: 200.}, consts2);
+        let mouse_temp = pt{x: 0., y: 0.};
+        let s = MainState { boids: vec![boid1, boid2], mouse: mouse_temp};
         Ok(s)
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
+        for boid in &mut self.boids {
+            boid.update(self.mouse);
+        }
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
-        graphics::circle(ctx,
-                         DrawMode::Fill,
-                         Point2::new(self.pos_x, 380.0),
-                         100.0,
-                         2.0)?;
+        let boid_mesh = boid_mesh(ctx)?;
+        for boid in &self.boids {
+            graphics::draw_ex(ctx, &boid_mesh,
+                              graphics::DrawParam {
+                                  dest: pt_gfx(boid.loc),
+                                  rotation: rads(boid.dir),
+                                  ..Default::default() })?;
+        }
         graphics::present(ctx);
         Ok(())
     }
-}
 
+    fn mouse_motion_event(&mut self,
+                          _ctx: &mut Context,
+                          _state: MouseState,
+                          x: i32,
+                          y: i32,
+                          _xrel: i32,
+                          _yrel: i32) {
+        self.mouse = pt { x: x as f32, y: y as f32 };
+    }
+
+    fn mouse_button_down_event(&mut self,
+                               _ctx: &mut Context,
+                               button: MouseButton,
+                               x: i32,
+                               y: i32) {
+        if button == MouseButton::Left{
+            let consts = BoidConstants::new(&mut thread_rng());
+            println!("new: {:?}", &consts);
+            let boid = Boid::new(pt{x: x as f32, y: y as f32}, consts);
+            self.boids.push(boid);
+        }
+
+    }
+}
 pub fn main() {
     let c = conf::Conf::new();
     let ctx = &mut Context::load_from_conf("super_simple", "ggez", c).unwrap();
